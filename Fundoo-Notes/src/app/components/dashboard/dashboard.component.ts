da6ab00dbc6c8fff9d5161efac0002e52e9f42f8
akshaya341 <akshayakumarmi97@gmail.com>
2019-02-05 19:22:30 +0530

    Designed UI for note with all lables

--- Fundoo-Notes/src/app/components/dashboard/dashboard.component.ts
+++ Fundoo-Notes/src/app/components/dashboard/dashboard.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
 import {MatDialog, MAT_DIALOG_DATA} from '@angular/material'; 
-
+import {MatExpansionModule} from '@angular/material/expansion';
+import { HttpService } from '../service/http.service';
+import { Router } from '@angular/router';
 
 /**
  * @title Injecting data when opening a dialog
@@ -17,13 +19,16 @@ import {MatDialog, MAT_DIALOG_DATA} from '@angular/material';
 
 export class DashboardComponent implements OnInit {
   mobileQuery: MediaQueryList;
+  panelOpenState = false;
+
 
   fillerNav = Array.from({length: 5}, (_, i) => `Nav Item ${i + 1}`);
   
   // 
   private _mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher,private httpService: HttpService, private router: Router) 
+  {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
@@ -38,11 +43,35 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
   }
-
-
+  
   isclick(){
     return false;
   }
 
+ 
+  step = 0;
 
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
+  
+  close(num:number){
+    this.setStep(num);
+  }
+
+  check(num:number){
+    this.setStep(num);
+  }
+
+  addAccount(){
+    this.router.navigate(['register']);
+  }
 }
